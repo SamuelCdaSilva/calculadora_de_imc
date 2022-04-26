0df224c94cf4b2fbcf265553e3768b34d2105e49
SamuelCdaSilva <91702874+SamuelCdaSilva@users.noreply.github.com>
2022-04-25 21:18:53 -0300

    JS

---
+++ IMC.js
@@ -0,0 +1,16 @@
+
+const calcular = document.getElementById("calcular");
+
+
+function imc() {
+    const nome = document.getElementById("nome");
+    const altura = document.getElementById("altura");
+    const peso = document.getElementById("peso");
+    const resultado = document.getElementById("resultado");
+
+    if (){
+        
+    }
+}
+
+calcular.addEventListener("click", imc);
