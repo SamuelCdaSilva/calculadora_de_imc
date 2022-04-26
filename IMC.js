ca9899800691638f1ce82022651db712d0724b07
SamuelCdaSilva <91702874+SamuelCdaSilva@users.noreply.github.com>
2022-04-26 02:31:14 -0300

    JS

--- IMC.js
+++ IMC.js
@@ -7,6 +7,12 @@ function imc() {
     const altura = document.getElementById("altura");
     const peso = document.getElementById("peso");
     const resultado = document.getElementById("resultado");
+
+    if (nome.value == "" || altura.value == "" || peso.value == ""){
+        alert("preencha todos os campos!");
+    } else {
+        alert("campos preenchidos!");
+    }
 }
 
 calcular.addEventListener("click", imc);
